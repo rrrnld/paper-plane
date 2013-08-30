3d487d00dcc2c62802092aeee1484baf3e2a8146
Arne Schlüter <aesthaddicts@gmail.com>
2013-08-30 02:29:21 +0200

    CodeMirror Ternserver running

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
-requirejs.config({
+require.config({
 	paths: {
 		'vendor/paper': 'vendor/paper/dist/paper-full',
 		'acorn': 'vendor/acorn'
 	}
 });
 
-require([ 'vendor/jquery/jquery', 'app.init', 'hoodie.singleton' ], function ( $, app, hoodie ) {
+require([
+  'vendor/jquery/jquery',
+  'app.init',
+  'hoodie.singleton'
+], function ( $, app, hoodie ) {
   app.init();
 });
--- src/js/editor.plugins.js
+++ src/js/editor.plugins.js
@@ -1,13 +1,15 @@
 // this file is used to load the necessary addons and plugins for codemirror
-require([
-	'vendor/tern/lib/tern',
+define([
 	'vendor/codemirror/addon/tern/tern',
+	'vendor/codemirror/addon/hint/show-hint',
 	'vendor/codemirror/mode/javascript/javascript'
 ], function (TernServer) {
+
 	return {
-		tern: new TernServer.Server({ 
+		tern: new TernServer({ 
 			useWorker: !!window.Worker,
-			workerScript: 'js/vendor/codemirror/addon/tern/worker.js'
+			workerScript: 'js/vendor/codemirror/addon/tern/worker.js',
+      worderDeps: [ '../../../acorn/acorn.js', '../../../tern/lib/tern.js' ] 
 		})
 	};
 });
